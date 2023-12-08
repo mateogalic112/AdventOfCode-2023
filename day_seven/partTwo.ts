e0fe07abce2d141d74010ed247d2b3d17f91553e
Mateo Galić <mateogalic112@gmail.com>
2023-12-08 13:54:24 +0100

    .

--- day_seven/partTwo.ts
+++ day_seven/partTwo.ts
@@ -46,53 +46,49 @@ const cardCheck = (hand: string) => {
   const handCheck = hand
     .split("")
     .reduce((acc: Record<string, number>, curr) => {
-      if (typeof acc[curr] === "undefined") {
-        acc[curr] = 1;
-      } else {
-        acc[curr]++;
-      }
+      acc[curr] = (acc[curr] || 0) + 1;
       return acc;
     }, {});
 
-  const handResult = Object.values(handCheck);
-  console.log({ handCheck });
+  const handResult = Object.entries(handCheck)
+    .filter((entry) => entry[0] !== "J")
+    .map((entry) => entry[1]);
+
+  const jokers = handCheck["J"] || 0;
 
   // five of a kind
-  if (handResult.find((v) => v === 5)) return 0;
-  if (handResult.find((v) => v === 4) && handCheck["J"] === 1) return 0;
-  if (handResult.find((v) => v === 3) && handCheck["J"] === 2) return 0;
-  if (handResult.find((v) => v === 2) && handCheck["J"] === 3) return 0;
-  if (handCheck["J"] === 4) return 0;
+  if (handResult.find((v) => v + jokers === 5)) {
+    return 0;
+  }
+
   // four of a kind
-  if (handResult.find((v) => v === 4)) return 1;
-  if (handResult.find((v) => v === 3) && handCheck["J"] === 1) return 1;
-  if (handResult.find((v) => v === 2) && handCheck["J"] === 2) return 1;
-  if (handCheck["J"] === 3) return 1;
+  if (handResult.find((v) => v + jokers === 4)) {
+    return 1;
+  }
+
   // full house
-  if ([3, 2].every((v) => handResult.includes(v))) return 2;
   if (
-    handResult.reduce((acc, curr) => {
-      if (curr === 2) acc++;
-      return acc;
-    }, 0) === 2 &&
-    handCheck["J"] === 1
-  )
+    [3, 2].every((v) => handResult.includes(v)) ||
+    (handResult.filter((v) => v === 2).length === 2 && jokers === 1)
+  ) {
     return 2;
+  }
+
   // three of a kind
-  if (handResult.find((v) => v === 3)) return 3;
-  if (handResult.find((v) => v === 2) && handCheck["J"] === 1) return 3;
-  if (handCheck["J"] === 2) return 3;
+  if (handResult.find((v) => v + jokers === 3)) {
+    return 3;
+  }
+
   // two pair
-  if (
-    handResult.reduce((acc, curr) => {
-      if (curr === 2) acc++;
-      return acc;
-    }, 0) === 2
-  )
+  if (handResult.filter((v) => v === 2).length === 2) {
     return 4;
+  }
+
   // one pair
-  if (handResult.find((v) => v === 2)) return 5;
-  if (handCheck["J"]) return 5;
+  if (handResult.find((v) => v === 2) || jokers) {
+    return 5;
+  }
+
   // high card
   return 6;
 };
